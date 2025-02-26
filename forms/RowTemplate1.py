58abe12bc5740f8d06d97fe180818d71a527b3e8
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-02-26 02:58:29 +0000

    Cloned app

---
+++ forms/RowTemplate1.py
@@ -0,0 +1,13 @@
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+from anvil.tables import app_tables
+import anvil.users
+
+class RowTemplate1(RowTemplate1Template):
+
+  def __init__(self, **properties):
+    # You must call self.init_components() before doing anything else in this function
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
