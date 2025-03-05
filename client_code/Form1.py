12614958c970b7e6f9cf26c43d50ea22b4c6250e
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-03-05 16:08:44 +0000

    Edited DatabaseProxy and Form1

--- client_code/Form1.py
+++ client_code/Form1.py
@@ -5,8 +5,8 @@ import anvil.tables as tables
 from anvil.tables import app_tables
 import anvil.users
 
-class Form1(Form1Template):
 
+class Form1(Form1Template):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
@@ -15,29 +15,35 @@ class Form1(Form1Template):
     anvil.users.login_with_form()
     self.refresh_data_bindings()
 
-    self.drop_down_channel.items = [(channel['name'], channel) for channel in anvil.server.call_s('get_channels')]
+    self.drop_down_channel.items = [
+      (channel["name"], channel) for channel in anvil.server.call_s("get_channels")
+    ]
     self.get_messages()
-  
+
   def current_user_email(self):
     if anvil.users.get_user() is not None:
-      return anvil.users.get_user()['email']
-    return 'Nobody'
-  
+      return anvil.users.get_user()["email"]
+    return "Nobody"
+
   def get_messages(self):
-    self.repeating_panel_1.items = anvil.server.call_s('get_messages', self.drop_down_channel.selected_value)
+    self.repeating_panel_1.items = anvil.server.call_s(
+      "get_messages", self.drop_down_channel.selected_value
+    )
 
   def button_send_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.send_message()
-    
+
   def text_box_message_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     self.send_message()
 
   def send_message(self):
-    anvil.server.call('add_message', self.drop_down_channel.selected_value, self.text_box_message.text)
+    anvil.server.call(
+      "add_message", self.drop_down_channel.selected_value, self.text_box_message.text
+    )
     self.get_messages()
-    self.text_box_message.text = ''
+    self.text_box_message.text = ""
 
   def drop_down_channel_change(self, **event_args):
     """This method is called when an item is selected"""
--- server_code/DatabaseProxy.py
+++ server_code/DatabaseProxy.py
@@ -7,18 +7,18 @@ from datetime import datetime
 
 @anvil.server.callable
 def get_channels():
-  if anvil.users.get_user():
-    return app_tables.channels.search()
-  return []
+    if anvil.users.get_user():
+        return app_tables.channels.search()
+    return []
 
 
 @anvil.server.callable
 def add_message(channel, message):
-  if anvil.users.get_user():
-    app_tables.messages.add_row(from_user=anvil.users.get_user(), message=message, when=datetime.now(), to_channel=channel)
+    if anvil.users.get_user():
+        app_tables.messages.add_row(from_user=anvil.users.get_user(), message=message, when=datetime.now(), to_channel=channel)
 
     
 @anvil.server.callable
 def get_messages(channel):
-  if anvil.users.get_user():
-    return app_tables.messages.search(tables.order_by('when', ascending=False), to_channel=channel)
+    if anvil.users.get_user():
+        return app_tables.messages.search(tables.order_by('when', ascending=False), to_channel=channel)
